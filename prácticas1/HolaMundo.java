d4a8e3e8624160fd74e638c959367c634709f94f
vandreu82 <92438198+vandreu82@users.noreply.github.com>
2021-10-20 16:32:03 +0200

    Update HolaMundo.java

--- prácticas1/HolaMundo.java
+++ prácticas1/HolaMundo.java
@@ -8,6 +8,6 @@ public class HolaMundo {
 	public static void main(String[] args) {	
 		System.out.println("Programa simple que hacen todos los programadores por primera vez");
 		System.out.println("Víctor Manuel Andreu Felipe - ver. 08/10/2020");
-		System.out.println("Hola mundo...");  //Muestra en pantalla el mensaje indicado
+		System.out.println("Hola mundo...");  //Muestra en pantalla el mensaje indicado asd
 	}
 }
