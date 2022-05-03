898f16af34636c5909c1e58d7f1169e4861dadae
dam21-01 <dam21-01@c16x10.iescierva.net>
2022-05-03 11:58:56 +0200

    asdasdad

---
+++ ejercicios/src/practicas02/Ejercicio08.java
@@ -0,0 +1,17 @@
+/**Escribe un programa simple que muestre en tres líneas separadas
+ tu nombre completo, calle, ciudad, país y código postal.*/
+ 
+ public class Ejercicio08 {
+	 public static void main(String[] args) {
+		 
+		 String dibujo = "";
+				 
+		 dibujo += "  |\\_/|\n";
+		 dibujo += " / @ @ \\\n";
+		 dibujo += "( > º < )\n";
+		 dibujo += "` »»x««´\n";
+		 dibujo += " /  O  \\\n";//sadsadasdas
+		 
+		 System.out.print(dibujo);
+	 }
+ }
